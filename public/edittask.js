c5c6b77728d478e3909dd457635abf5983ba0890
Vyshnavi Santhosh <vyshnaviponnuzz0001@gmail.com>
2023-01-12 09:06:14 +0530

    updated task

--- public/edittask.js
+++ public/edittask.js
@@ -10,3 +10,21 @@ async function getOneTask() {
   }
 }
 getOneTask();
+
+let taskName = document.querySelector("#taskname");
+// console.log(taskName);
+let taskDescrip = document.querySelector("#taskdescrip");
+// console.log(taskDescrip);
+let form = document.querySelector("#form");
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  let res = await axios.patch("http://localhost:5000/update-one-task/" + id, {
+    task_name: taskName.value,
+    task_descrip: taskDescrip.value,
+  });
+  console.log(res.data);
+  if (res.data.sucess) {
+    alert("Updated Task Successfully");
+    window.location.href = "http://localhost:5000/index.html";
+  }
+});
